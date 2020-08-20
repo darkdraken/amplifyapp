1372327c5ecd2160bca723fbfe0a633ab68aa73b
Juan Arbelaez <jarbelaez@growthaccelerationpartners.com>
2020-08-20 17:34:04 -0500

    Se agrega auth con cognito

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from "./app/app.module";
+import config from "./aws-exports";
+import { environment } from "./environments/environment";
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import Amplify from "aws-amplify";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+Amplify.configure(config);
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
